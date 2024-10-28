cb8b8d39972d5c2cf98bf943ed0c12a82c60236b
itsmht <iamtalha.mht@gmail.com>
2024-10-28 11:46:06 +0600

    added schemes table

---
+++ database/migrations/2024_10_28_105416_create-schemes-table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schemes', function (Blueprint $table) {
+            $table->bigIncrements('scheme_id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('title');
+            $table->double('price');
+            $table->double('winning_price');
+            $table->string('status');
+            $table->foreign('user_id')->references('user_id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
