c4df83daf84b7fd8612a6e1057c34d23f18a1519
itsmht <iamtalha.mht@gmail.com>
2024-11-09 11:08:37 +0600

    rules page changed

--- resources/views/user/rules.blade.php
+++ resources/views/user/rules.blade.php
@@ -13,40 +13,24 @@
 
 	<h1>Rules</h1>
 
-	<p>Brush up on the Lotto India rules before you start playing to make sure you know exactly how the game works. If you’re looking for more information about playing Lotto India or choosing your numbers, head over to the <a href="how-to-play.html" title="How to Play Lotto India">How to Play</a> page.</p>
+	<p>Brush up on the Asian Lottery rules before you start playing to make sure you know exactly how the game works. If you’re looking for more information about playing The Asian Lottery or choosing your numbers, head over to the <a href="{{route('how')}}" title="How to Play Lotto India">How to Play</a> page.</p>
 	
 	<div class="h3">Registration and Account</div>
 	<ul class="list">
-		<li>Email addresses used to register with Lotto-India.com need to be verified by the owner of the email account after registering. Certain features or functions of Lotto-India.com may not be available to a user until they have completed this verification process.</li>
-		<li>Only one account is allowed per user and per email address. If multiple accounts are found to be under one person then we reserve the right to block this person and these accounts from winning any prizes.</li>
+		<li>Phone number is used to register with The Asian Lottery. Certain features or functions of The Asian Lottery may not be available to a user until they have completed this registration process.</li>
+		<li>Only one account is allowed per user and per phone number. If multiple accounts are found to be under one person then we reserve the right to block this person and these accounts from winning any prizes.</li>
 		<li>Entrants must be 18 or over.</li>
 	</ul><br>
 
 	<div class="h3">Entering Draws</div>
 	<ul class="list">
-		<li>Tickets are electronically stored in your Free Lottery account and are only valid for the draw stated on that individual ticket.</li>
-		<li>Entrants are only allowed one entry in each draw.</li>
-		<li>By registering for an account at Free-Lottery.net, participants agree to participate in publicity surrounding the game should they win a prize. This may include providing photographs and a quote for use on Free-Lottery.net. Larger prizes may be presented in person for publicity purposes.</li>
-		<li>Lotto-India.com and its agents and partners can limit or refuse entries to anyone at their discretion and without giving notice or reason.</li>
-		<li>It is the player’s responsibility to ensure that they are legally eligible to enter Lotto India. Any prizes won may be forfeited if the winner is not legally eligible to participate.</li>
-		<li>Sales of entries close at 5:50 PM on the day of each draw. Panels that have not been completed and that have not been activated by this time will not be considered as entries for that day’s draw.</li>
+		<li>For daily lottery you have to choose 1 digit from 1 to 99...From any of the following category like 100,200,300,500,1000.</li>
+		<li>For weekly lottery you have to choose two 2 digit From 1 to 99 and submit From category 5000 and 10000.</li>
+		<li>For monthly lottery you have to hoose any 3 digit From 1 to 99 and submitt it.</li>
+		<li>Make sure you buy lottery from this site and check the payment number before you send the lottery money.</li>
+		<li>You can buy unlimited number from any categories like 100 to 10000.</li>
 	</ul><br>
 
-	<div class="h3">The Draw</div>
-	<ul class="list">
-		<li>In every draw, an Insured Random Number Generator (iRNG) selects six numbers between 1 and 75 (the ‘main numbers’). These randomly selected numbers constitute the winning numbers (the ‘winning line’) for that draw.</li>
-		<li>The time at which the Lotto India draw takes place may change at any time at the discretion of Lotto-India.com and its partners.</li>
-		<li>Draws are not conducted live and cannot be viewed as they are completed by a random number generator that is not controlled by Lotto India com staff. This is to make sure that the draws are random and not influenced by any party.</li>
-	</ul><br>
-	
-	<div class="h3">Prizes and Payments</div>
-	<ul class="list">
-		<li>Players can only win the amounts detailed on the <a href="prizes.html" title="Lotto India Prizes">Prizes</a> page of Lotto-India.com.</li>
-		<li>The jackpot prize of ₹1 Lakh will be split equally between all players in the same draw who match six numbers on one of their panels.</li>
-		<li>The results shown by the Lotto India <a href="check-numbers.html" title="Ticket Checker">Ticket Checker</a> do not act as proof of winning; players must have a valid entry matching sufficient numbers to be eligible for a prize payout.</li>
-		<li>Prize money will be paid into a winner’s bank account once the ID verification is complete and the details of your account match your identity.</li>
-	</ul>
-  
 </div>
 
 	</div>
